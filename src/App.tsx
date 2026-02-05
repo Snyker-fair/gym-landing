a6bf6d63e2ee159c713dc53112a7ea4617d14adf
Kevin Pastén <kevin.pastenr@gmail.com>
2026-02-05 04:05:00 -0300

    Fix: Desactivar check estricto de TS en build 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Navigation from './components/layout/Navbar';
 import Hero from './components/sections/Hero';
 import DisciplinesSection from './components/sections/DisciplinesSection';
